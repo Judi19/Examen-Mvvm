956ae8233cffbdbfae4c4f8b162a92cc64c3217c
Judi19 <167385372+Judi19@users.noreply.github.com>
2024-12-11 18:28:31 -0600

    Agregar archivos de proyecto.

---
+++ Examen-Mvvm/Examen-Mvvm.Droid/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace Examen_Mvvm.Droid
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Examen-Mvvm/Examen-Mvvm.Mac/AppDelegate.cs
@@ -0,0 +1,10 @@
+using Foundation;
+
+namespace Examen_Mvvm.Mac
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

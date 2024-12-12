0760d95c2bc0663ce87738abb2c620ef6c8a39a6
Judi19 <167385372+Judi19@users.noreply.github.com>
2024-12-11 18:59:57 -0600

    Create TotalPagar.cs

---
+++ Examen-Mvvm/Examen-Mvvm.Droid/TotalPagar.cs
@@ -0,0 +1,15 @@
+namespace Examen_Mvvm;
+
+public class TotalPagar : ContentPage
+{
+	public TotalPagar()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
